6fc34b6f3ca21ab81de53c9272ea61d5eaeb19c0
Shrawan Shinde <syntaxerror1972@gmail.com>
2022-09-15 01:55:22 +0530

    Added lib

--- app/src/main/java/com/syntaxerror/myfirstlibtoast/MainActivity.java
+++ app/src/main/java/com/syntaxerror/myfirstlibtoast/MainActivity.java
@@ -3,12 +3,24 @@ package com.syntaxerror.myfirstlibtoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShow=(Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
